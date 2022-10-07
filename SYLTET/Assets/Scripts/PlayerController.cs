b6c21413a2954488972ac89be42551f1100a3469
simon <simon.h.oscarson@hotmail.se>
2022-10-07 19:52:00 +0200

    Move & Jump inProgress!

--- SYLTET/Assets/Scripts/PlayerController.cs
+++ SYLTET/Assets/Scripts/PlayerController.cs
@@ -12,23 +12,51 @@ public class PlayerController : MonoBehaviour
     public int jumpForce;
     public UnityEvent FireEvent;
     public UnityEvent JumpEvent;
-    private bool isGrounded;
+    [SerializeField] private float jumpCD = 0.8f;
+    private float timer = 1;
+    private bool moveLeft = true;
+    
+    [SerializeField] private IsGrounded groundCheck;
 
     private void FixedUpdate()
     {
         transform.Translate(new Vector3(moveInp.x, transform.position.y, 0) * speed * Time.deltaTime);
+        
+
+
+    }
+    private void Update()
+    {
+        if (moveInp.x > 0)
+        {
+            moveLeft = true;
+        }
+        else
+        {
+            moveLeft = false;
+        }
+        if (moveLeft)
+        {
+            transform.Rotate(0, 0, 0);
+        }
+        else
+        {
+            transform.Rotate(0, 180, 0);
+        }
+        timer += Time.deltaTime;
     }
     public void OnMove(InputAction.CallbackContext ctx) => moveInp = ctx.ReadValue<Vector2>();
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (isGrounded)
+        
+        if (groundCheck.isGrounded && timer > jumpCD)
         {
             rB = gameObject.GetComponent<Rigidbody>();
-            rB.velocity = Vector3.zero;
             rB.AddForce(0, 1 * jumpForce, 0);
             Debug.Log("Jump!");
             JumpEvent.Invoke();
+            timer = 0;
         }
        
        
@@ -39,19 +67,6 @@ public class PlayerController : MonoBehaviour
         FireEvent.Invoke();
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag.Equals("ground"))
-        {
-            isGrounded = true;  
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag.Equals("ground"))
-        {
-            isGrounded = false;
-        }
-    }
+   
 
 }
