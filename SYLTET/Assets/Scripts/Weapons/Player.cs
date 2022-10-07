69536dd97dc29009365c103c7d0555fe7ce6018e
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-10-07 18:54:33 +0200

    .

--- SYLTET/Assets/Scripts/Weapons/Player.cs
+++ SYLTET/Assets/Scripts/Weapons/Player.cs
@@ -4,4 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Weapon weapon;
+    private void Start()
+    {
+        weapon = gameObject.GetComponentInChildren<Weapon>();
+    }
+
+    private void Update()
+    {
+        Shoot();
+    }
+    public void Shoot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            weapon.Shoot();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            weapon.timer = 1;
+        }
+    }
 }
