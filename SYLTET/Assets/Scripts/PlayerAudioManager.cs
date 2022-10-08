7b7171ba3aca9fd215a63411f91aed8e4cb8b6ed
Arxhlight <102960157+Arxhlight@users.noreply.github.com>
2022-10-08 12:28:39 +0200

    Clean project

--- SYLTET/Assets/Scripts/PlayerAudioManager.cs
+++ SYLTET/Assets/Scripts/PlayerAudioManager.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerAudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private AudioSource source;
+    [SerializeField] AudioClip[] sounds;
+
+
+    [Header("Attributes")]
+    [Range(0f, 2f)]
+    public float volume;
+    [Range(1f, 5f)]
+    public float pitch;
+    [Range(0f, 1f)]
+    public float spatialBlend;
+
+    [Header("PitchMinMax")]
+    [SerializeField] bool isPitchAllowed;
+    [Range(1f, 5f)]
+    public float pitchMin;
+    [Range(1f, 5f)]
+    public float pitchMax;
+
+    private void Start()
     {
-        
+        source = GetComponent<AudioSource>();
+        source.pitch = pitch;
+        source.volume = volume;
+        source.spatialBlend = spatialBlend;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+
+    }
+
+    private void playerMoveSounds()
+    {
+        // Whatever the controlls for sertain moves
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (isPitchAllowed)
+            {
+                source.pitch = Random.Range(pitchMin, pitchMax);
+            }
+
+            source.PlayOneShot(source.clip = sounds[Random.Range(0, sounds.Length)]);
+        }
     }
 }
